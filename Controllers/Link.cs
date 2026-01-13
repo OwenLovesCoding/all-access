a509d9c05e5736b7f86ce6a90447bfa901dceb7c
unknown <iraoyaowen5@gmail.com>
2026-01-13 16:15:56 +0000

    first commit

---
+++ Controllers/Link.cs
@@ -0,0 +1,51 @@
+﻿using all_access.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System.Text;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace all_access.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Link(IHttpClientFactory _httpClient, IConfiguration configuration)
+    {
+        private readonly IHttpClientFactory _httpClientFactory = _httpClient;
+        private readonly IConfiguration _configuration = configuration;
+
+
+        private readonly string rebrandlyApiKey = configuration["Rebrandly:ApiKey"]!;
+        private readonly string rebrandlyWorkSpaceId = configuration["Rebrandly:WorkSpaceId"]!;
+
+        [HttpPost("send-link")]
+        public async Task<string> ShortenLink([FromBody] Create_Link urlStruc)
+        {
+            Console.WriteLine(urlStruc);
+            return "";
+
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+
+                client.DefaultRequestHeaders.Add("apikey", rebrandlyApiKey);
+                client.DefaultRequestHeaders.Add("workspace", rebrandlyWorkSpaceId);
+
+                var urlString = new StringContent(JsonSerializer.Serialize(urlStruc), Encoding.UTF8, Application.Json);
+
+                using var httpResponseMessage = await client.PostAsync("https://api.rebrandly.com/v1/links", urlString);
+
+                var responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+                return $"{responseContent}";
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception("Error creating HTTP request", ex);
+            }
+       }
+    }
+}
---
+++ Models/Create-Link.cs
@@ -0,0 +1,19 @@
+﻿namespace all_access.Models
+{
+    public class Create_Link
+    {
+        //string url = null!;
+       public string SourceUrl { 
+        
+            get { return SourceUrl; }
+            set { 
+                if (value.Length < 6 && string.IsNullOrEmpty(value))
+             
+                { 
+                    
+                    throw new ArgumentException("Invalid URL"); 
+                }
+            }
+        }
+    }
+}
